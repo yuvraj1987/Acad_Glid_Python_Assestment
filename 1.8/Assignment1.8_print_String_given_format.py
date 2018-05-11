9e7795cb23976221fa1ad8fee67e01c5bd276510
Jitendra <jdjkrish1987@gmail.com>
2018-05-11 20:38:04 +0530

    Add files via upload

---
+++ 1.8/Assignment1.8_print_String_given_format.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 11 14:45:38 2018
+
+@author: jkdadhich
+"""
+"""
+NOTE :-  This is just wild guess to obtain desired result
+         Using \n for next line and \t tab space
+         No Condition is given here like after ',' print next line
+         with tab space etc..
+"""
+
+print ("~*~"*20)
+print (""" "Below is Sample provided in Assignment" """)
+print ("~*~"*20)
+print()
+Sample_Input = "WE, THE PEOPLE OF INDIA, having Solemnly resolved to contitue India into a\
+SOVERIGN, SOCIALIST, SECULAR, DEMOCRATRIC REPUBLIC and to Secure to all its citizens"
+
+print (Sample_Input)
+print()
+print ("~*~"*20)
+print (""" "Desired Output of Sample Data" """)
+print ("~*~"*20)
+Sample_Output = str("WE, THE PEOPLE OF INDIA,\n\t having Solemnly resolved to contitue India into a SOVERIGN, SOCIALIST,! \n\t\tSECULAR, DEMOCRATRIC REPUBLIC\n\t\t and to Secure to all its citizens")
+
+print (Sample_Output)
+
+print ("~*~"*20)
