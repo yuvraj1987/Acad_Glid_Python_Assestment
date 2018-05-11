c9a937bde6ff7a4c37cb0aece7443bfdaf78bde9
Jitendra <jdjkrish1987@gmail.com>
2018-05-11 14:38:32 +0530

    Add files via upload

---
+++ 1.7/Assignment1.6_String_Reverse.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 11 14:11:16 2018
+
+@author: jkdadhich
+"""
+print()
+print ("~*~"*15)
+# Asking User to Input Value and Converting into String Format
+User_input = str(input(" Please Enter the String or Word :\t" ))
+print ("~*~"*8)
+print (" You have Enter :\t",User_input) # Printing the same word which user Enter
+print ("~*~"*8)
+print (" Your Result Is :\t",User_input[::-1]) # Printing the word in reverse order
+print ("~*~"*15)
